4031c84fc03f4a8eb39f5caa0b9acd5fcdc44e27
Southern Wind <2060518839@qq.com>
2025-03-21 16:34:24 +0800

    Updates

---
+++ assets/api-dcf90377.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index-ab11327c.js";const r=e("api",{state:()=>({apiUrl:"http://106.14.176.242:9998/v1/chat/completions"}),actions:{setApiUrl(t){this.apiUrl=t,localStorage.setItem("apiUrl",t)},getApiUrl(){return this.apiUrl}}});export{r as u};
