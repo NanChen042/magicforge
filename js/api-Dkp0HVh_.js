9e0230c2706b036bcc430abcd74fc6542b4058ab
Southern Wind <2060518839@qq.com>
2025-03-24 10:20:26 +0800

    Updates

--- js/api-Dkp0HVh_.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./pinia-B-GcKEsP.js";const r=e("api",{state:()=>({apiUrl:"http://106.14.176.242:9998/v1/chat/completions"}),actions:{setApiUrl(t){this.apiUrl=t,localStorage.setItem("apiUrl",t)},getApiUrl(){return this.apiUrl}}});export{r as u};
